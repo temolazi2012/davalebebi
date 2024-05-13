3c84439bd0237311e145527211621414f5f92f50
temolazi2012 <lazishvilitemo@gmail.com>
2024-05-13 21:37:10 +0300

    second commit

---
+++ day 02/homework.py
@@ -0,0 +1,5 @@
+name1 = "luka modebadze"
+my_name = "temo lazishvili"
+name2 = "vano motiashvili"
+name3 = "nika keshelava"
+print("chemi saxelia ," , my_name , "es academia vurchie ," , name1+"s" , "chemi lideria ," , name2 , "chemi maswavlebelia ," , name3)
